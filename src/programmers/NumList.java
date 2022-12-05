ca2cd3af8b5bd47e6e7502a08305b57fc7f6aa5e
Lee Sol <leeso4567@naver.com>
2022-12-05 09:32:06 +0900

    n의 배수 고르기

---
+++ src/programmers/NumList.java
@@ -0,0 +1,27 @@
+package programmers;
+
+import java.util.ArrayList;
+
+//프로그래머스 코딩테스트 입문 <n의 배수 고르기>
+public class NumList {
+    public ArrayList<Integer> solution(int n, int[] numlist) {
+        ArrayList<Integer> answer = new ArrayList<>();
+
+        for(int x : numlist) {
+            if(x % n == 0) {
+                answer.add(x);
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        NumList numList = new NumList();
+        int[] numlist = {4, 5, 6, 7, 8, 9, 10, 11, 12};
+        for(int x : numList.solution(3, numlist)){
+            System.out.print(x + " ");
+        }
+
+    }
+}
