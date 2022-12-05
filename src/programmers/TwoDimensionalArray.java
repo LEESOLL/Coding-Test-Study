2027f4152bd8d0d52e215cec9b4ce1522c58fc96
Lee Sol <leeso4567@naver.com>
2022-12-05 20:21:12 +0900

    2차원으로 만들기 통과코드

--- src/programmers/TwoDimensionalArray.java
+++ src/programmers/TwoDimensionalArray.java
@@ -3,24 +3,29 @@ package programmers;
 import java.util.Arrays;
 
 public class TwoDimensionalArray {
-    public static int[][] solution (int[] num_list, int n) {
+    public int[][] solution (int[] num_list, int n) {
         int len = num_list.length/n;
-        int[][] answer = new int[1][len];
-
+        int idx = 0;
+        int[][] answer = new int[len][n];
+        for(int i = 0; i < len; i++) {
+            for(int j = 0; j < n; j++) {
+                answer[i][j] = num_list[idx++];
+            }
+        }
         return answer;
     }
 
     public static void main(String[] args) {
+        TwoDimensionalArray twoDimensionalArray = new TwoDimensionalArray();
         int[] num_list = {1, 2, 3, 4, 5, 6, 7, 8};
         int n = 2;
-        int[][] arr = solution(num_list, n);
+        int[][] arr = twoDimensionalArray.solution(num_list, n);
 
-        System.out.println(Arrays.stream(arr).toArray());
-
-//        for(int i = 0; i < arr.length; i++) {
-//            for(int j = 0; j < arr[i].length; j++) {
-//                System.out.println(arr[i][j] + " ");
-//            }
-//        }
+        for(int i = 0; i < arr.length; i++) {
+            for(int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
