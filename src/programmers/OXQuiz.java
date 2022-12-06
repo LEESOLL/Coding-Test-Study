0e4e3cbb792df073e925f405d3ab1ba8514aea8c
Lee Sol <leeso4567@naver.com>
2022-12-06 14:18:13 +0900

    OX 퀴즈 런타임 에러난 코드

---
+++ src/programmers/OXQuiz.java
@@ -0,0 +1,17 @@
+package programmers;
+import java.util.Stack;
+
+public class OXQuiz { //프로그래머스 코딩테스트 입문 <OX퀴즈>
+    public String[] solution(String[] quiz) {
+        String[] answer = new String[quiz.length];
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+       OXQuiz oxQuiz = new OXQuiz();
+       String[] quiz = {"3 - 4 = -3", "5 + 6 = 11"};
+        System.out.println(oxQuiz.solution(quiz)[0]);
+        System.out.println(oxQuiz.solution(quiz)[1]);
+    }
+}
