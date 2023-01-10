b24aa10351f9df6757da3224867c25416dadf766
Lee Sol <leeso4567@naver.com>
2023-01-10 13:44:39 +0900

    프로그래머스 코딩테스트 입문 <가장 큰 수 찾기> 통과코드

---
+++ src/programmers/FindMaxNum.java
@@ -0,0 +1,22 @@
+package programmers;
+
+public class FindMaxNum { //프로그래머스 입문 <가장 큰 수 찾기>
+    //https://school.programmers.co.kr/learn/courses/30/lessons/120899
+    public static int[] solution(int[] array) {
+        int[] answer = new int[2];
+
+        for(int i = 0; i < array.length; i++) {
+            if(array[i] > answer[0]) {
+                answer[0] = array[i];
+                answer[1] = i;
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
