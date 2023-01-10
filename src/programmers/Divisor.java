8dc2b60a6afddd2985b92853cd7a99b84717cbaa
Lee Sol <leeso4567@naver.com>
2023-01-10 13:50:07 +0900

    프로그래머스 입문 <약수 구하기> 통과코드

---
+++ src/programmers/Divisor.java
@@ -0,0 +1,22 @@
+package programmers;
+
+import java.util.ArrayList;
+
+public class Divisor { //프로그래머스 입문 <약수 구하기>
+    // https://school.programmers.co.kr/learn/courses/30/lessons/120897
+
+    public static ArrayList<Integer> solution(int n) {
+        ArrayList<Integer> answer = new ArrayList<>();
+
+        for(int i = 1; i <= n; i++) {
+            if(n % i == 0)
+                answer.add(i);
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
