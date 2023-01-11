213da28657ff7a34f8f3d4b6af0e1aa05051453d
Lee Sol <leeso4567@naver.com>
2023-01-11 23:01:35 +0900

    프로그래머스 입문 <배열 회전시키기> deque 사용한 풀이

--- src/programmers/MoveArray.java
+++ src/programmers/MoveArray.java
@@ -1,24 +1,38 @@
 package programmers;
 
+import java.util.ArrayDeque;
+
 public class MoveArray { //프로그래머스 입문 <배열 회전시키기>
     // https://school.programmers.co.kr/learn/courses/30/lessons/120844
-    public static int[] solution(int[] numbers, String direction) {
-        int[] answer = new int[numbers.length];
+    public static ArrayDeque<Integer> solution(int[] numbers, String direction) {
 
-        if(direction.equals("right")) {
-            answer[0] = numbers[numbers.length-1];
-            for(int i = 1; i < numbers.length; i++) {
-                answer[i] = numbers[i - 1];
-            }
+        ArrayDeque<Integer> deque = new ArrayDeque<>();
+        for(int x : numbers) {
+            deque.add(x); //numbers 의 원소를 모두 deque 에 넣어줌
+        }
+        if (direction.equals("right")) {
+            deque.addFirst(deque.removeLast());
         }
         else {
-            answer[answer.length-1] = numbers[0];
-            for(int i = 0; i < answer.length-1; i++) {
-                answer[i] = numbers[i + 1];
-            }
+            deque.addLast(deque.removeFirst());
         }
+        return deque;
 
-        return answer;
+        //        int[] answer = new int[numbers.length];
+//        if(direction.equals("right")) {
+//            answer[0] = numbers[numbers.length-1];
+//            for(int i = 1; i < numbers.length; i++) {
+//                answer[i] = numbers[i - 1];
+//            }
+//        }
+//        else {
+//            answer[answer.length-1] = numbers[0];
+//            for(int i = 0; i < answer.length-1; i++) {
+//                answer[i] = numbers[i + 1];
+//            }
+//        }
+
+//        return answer;
     }
 
     public static void main(String[] args) {
