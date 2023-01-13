dac445c092b14240be690340f74ccbe92abf5ccf
Lee Sol <leeso4567@naver.com>
2023-01-13 16:42:41 +0900

    프로그래머스 입문 <합성수 찾기>

--- src/programmers/CompositeNum.java
+++ src/programmers/CompositeNum.java
@@ -1,4 +1,26 @@
 package programmers;
 
-public class CompositeNum {
+public class CompositeNum { // 프로그래머스 입문 <합성수 찾기>
+    // https://school.programmers.co.kr/learn/courses/30/lessons/120846
+    public static int solution(int n) {
+        int answer = 0;
+        int count;
+        for(int i = 4; i <= n+159; i++) {
+            count = 0;
+            for(int j = 1; j <= i; j++) {
+                if(i % j == 0) {
+                    count++;
+                    if(count > 2) {
+                        answer++;
+                        break;
+                    }
+                }
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(10));
+    }
 }
