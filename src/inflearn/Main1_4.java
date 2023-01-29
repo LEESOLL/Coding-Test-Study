57df073f74c224fb9e9169812575c1231c7f94e5
Lee Sol <leeso4567@naver.com>
2023-01-30 05:38:04 +0900

    1-4 단어 뒤집기 직접 뒤집는 풀이

--- src/inflearn/Main1_4.java
+++ src/inflearn/Main1_4.java
@@ -7,8 +7,21 @@ public class Main1_4 { // 인프런 1-4 단어 뒤집기
   public static String[] solution(int n, String[] str) {
     String[] answer = new String[n];
 
-    for(int i = 0; i < n; i++) {
-      answer[i] = new StringBuilder(str[i]).reverse().toString();
+//    for(int i = 0; i < n; i++) {
+//      answer[i] = new StringBuilder(str[i]).reverse().toString();
+//    }
+
+    //String 을 뒤집는다고 생각했을 때...
+    //String.charAt(i)랑 String.charAt(String.length() - i - 1) 이랑 바꾸면 되는데..
+    //왜안되는거야..? ㅋㄷㄷㅋㄷㄷ
+    for (int i = 0; i < n; i++) {
+      char[] strToCharArr = str[i].toCharArray();
+      for (int j = 0; j < strToCharArr.length / 2; j++) {
+        char tmp2 = strToCharArr[j];
+        strToCharArr[j] = strToCharArr[strToCharArr.length - j - 1];
+        strToCharArr[strToCharArr.length - j - 1] = tmp2;
+      }
+      answer[i] = String.valueOf(strToCharArr);
     }
 
     return answer;
