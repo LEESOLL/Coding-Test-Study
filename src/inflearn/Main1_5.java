4afca743f8c67ff98837d5973e3635662dbc9641
Lee Sol <leeso4567@naver.com>
2023-01-31 15:27:50 +0900

    1-5 특정 문자 뒤집기

--- src/inflearn/Main1_5.java
+++ src/inflearn/Main1_5.java
@@ -1,2 +1,40 @@
-package inflearn;public class Main1_5 {
+package inflearn;
+
+import java.util.Scanner;
+
+public class Main1_5 { // 인프런 1-5 특정 문자 뒤집기
+    public static String solution(String str) {
+        // isCharacter 이용해서 문자 판별 후 left 랑 right 랑 바꿔주기
+        // left < right 일 때까지만 탐색
+        // if left 가 알파벳인 경우 -> if right 도 알파벳이면 둘이 바꿔
+        // if left 만 알파벳인 경우 -> right 하나 감소시켜
+        // if right 만 알파벳인 경우 -> left 하나 증가시켜
+        int left = 0;
+        int right = str.length() - 1;
+        char[] charArr = str.toCharArray();
+        while (left < right) {
+            if(Character.isAlphabetic(charArr[left])) {
+              if(Character.isAlphabetic(charArr[right])) {
+                char tmp = charArr[left];
+                charArr[left] = charArr[right];
+                charArr[right] = tmp;
+                left++;
+                right--;
+              }
+              else {
+                right--;
+              }
+            } else {
+              left++;
+            }
+        }
+
+        return String.valueOf(charArr);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        System.out.println(solution(str));
+    }
 }
--- src/programmers/LengthOfLine.java
+++ src/programmers/LengthOfLine.java
@@ -1,2 +1,48 @@
-package programmers;public class LengthOfLine {
+package programmers;
+import java.util.ArrayList;
+
+public class LengthOfLine { //프로그래머스 코딩테스트 입문 <겹치는 선분의 길이>
+    public int solution(int[][] lines) { //{{0, 5}, {3, 9}, {1, 10}}
+        ArrayList<Integer> arr = new ArrayList<>();
+        int answer = 0;
+        int min = 0;
+
+        for(int i = 0; i < lines.length; i++) {
+            for(int j = 0; j < lines[i].length; j++) {
+                if(lines[i][j] < min) {
+                    min = lines[i][j];
+                }
+            }
+        }
+        for(int i = 0; i < lines.length; i++) {
+            for(int j = 0; j < lines[i].length; j++) {
+                lines[i][j] += Math.abs(min);
+            }
+        } //모든 선분의 좌표가 0이상의 값을 갖도록 옮겨줌 (인덱스 연산 이용하기 위함)
+
+        for(int i = 0; i < lines.length; i++) {
+            for(int j = lines[i][0]; j <= lines[i][1]; j++) {
+                arr.add(j, 1);
+            }
+        }
+
+        for(int i = 0; i < lines.length; i++) {
+            for(int j = lines[i][0]; j <= lines[i][1]; j++) {
+                arr.add(j, arr.get(j) + 1);
+            }
+        }
+
+        for(int x : arr) {
+            if(x > 1)
+                answer++;
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        LengthOfLine lengthOfLine = new LengthOfLine();
+        int[][] lines = {{0, 2}, {-3, -1}, {-2, 1}};
+        System.out.println(lengthOfLine.solution(lines));
+    }
 }
