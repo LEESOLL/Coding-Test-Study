a10e1a7f3a27be0d7d66264fb797bd4816ab44fd
Lee Sol <leeso4567@naver.com>
2023-02-03 17:51:24 +0900

    1-6 중복문자제거 통과코드

---
+++ src/inflearn/Main1_6.java
@@ -0,0 +1,22 @@
+package inflearn;
+
+import java.util.Scanner;
+
+public class Main1_6 { // 인프런 1-6 중복문자제거
+    static StringBuilder solution(String str) {
+        StringBuilder answer = new StringBuilder();
+
+        while(str.length() > 0) {
+            answer.append(str.charAt(0));
+            str = str.replaceAll(String.valueOf(str.charAt(0)), "");
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        System.out.println(solution(str));
+    }
+}
