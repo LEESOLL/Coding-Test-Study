95766a70676726681193eb7247b0ab97c21fe899
Lee Sol <leeso4567@naver.com>
2023-03-02 11:47:40 +0900

    1-11 문자열 압축 통과코드

--- src/inflearn/Main1_11.java
+++ src/inflearn/Main1_11.java
@@ -1,2 +1,30 @@
-package inflearn;public class Main1_11 {
+package inflearn;
+
+import java.util.Scanner;
+
+public class Main1_11 { // 인프런 1-11 문자열 압축
+
+    static String solution(String str) {
+        String answer = "";
+        int cnt = 1;
+        str = str + ' ';
+        for(int i = 0; i < str.length() - 1; i++) {
+            if(str.charAt(i+1) == str.charAt(i)) {
+                cnt++;
+            } else {
+                answer += str.charAt(i);
+                if (cnt > 1) {
+                    answer += String.valueOf(cnt);
+                }
+                cnt = 1;
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        System.out.println(solution(str));
+    }
 }
