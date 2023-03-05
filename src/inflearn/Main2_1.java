0668c8ac9f0310ca404c2f696edbb56be2695360
Lee Sol <leeso4567@naver.com>
2023-03-05 15:03:54 +0900

    2-1 큰 수 출력하기 통과코드

---
+++ src/inflearn/Main2_1.java
@@ -0,0 +1,31 @@
+package inflearn;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main2_1 { // 인프런 2-1.큰 수 출력하기
+    static ArrayList<Integer> solution(int n, int[] arr) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        answer.add(arr[0]);
+        for(int i = 1; i < arr.length; i++) {
+            if(arr[i] > arr[i-1]) {
+                answer.add(arr[i]);
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for(int x : solution(n, arr)) {
+            System.out.print(x+" ");
+        }
+    }
+}
