6e2c315741fda41bd9e1905a5150f766974ef702
Lee Sol <leeso4567@naver.com>
2023-04-07 00:11:11 +0900

    [inflearn] 3-2. 공통원소구하기 수정

--- src/inflearn/Main3/Main3_2.java
+++ src/inflearn/Main3/Main3_2.java
@@ -6,36 +6,46 @@ import java.util.Scanner;
 
 public class Main3_2 { // 인프런 3-2. 공통원소구하기
 
-    static ArrayList<Integer> solution(int n, int m, int[] a, int[] b) {
+    static ArrayList<Integer> solution(int n, int[] arr1, int m, int[] arr2) {
         ArrayList<Integer> answer = new ArrayList<>();
-        int p1 = 0, p2 = 0;
-        Arrays.sort(a);
-        Arrays.sort(b);
-        while(p1<n && p2<m) {
-            if(a[p1]==b[p2]) {
-                answer.add(a[p1]);
-                p1++;p2++;
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+
+        int pn = 0;
+        int pm = 0;
+
+        while(pn < n && pm < m) {
+            if(arr1[pn] == arr2[pm]) {
+                answer.add(arr1[pn]);
+                pn++;
+                pm++;
+            } else if(arr1[pn] < arr2[pm]) {
+                pn++;
+            } else if(arr1[pn] > arr2[pm]) {
+                pm++;
             }
-            else if(a[p1]<b[p2])
-                p1++;
-            else
-                p2++;
         }
+
         return answer;
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] a = new int[n];
-        for (int i = 0; i < n; i++)
-            a[i] = sc.nextInt();
-        int m = sc.nextInt();
-        int[] b = new int[m];
-        for (int i = 0; i < m; i++)
-            b[i] = sc.nextInt();
 
-        for (int x : solution(n, m, a, b))
+        int n = sc.nextInt();
+        int[] arr1 = new int[n];
+        for(int i = 0; i < n; i++) {
+            arr1[i] = sc.nextInt();
+        }
+
+        int m = sc.nextInt();
+        int[] arr2 = new int[m];
+        for(int i = 0; i < m; i++) {
+            arr2[i] = sc.nextInt();
+        }
+
+        for(int x : solution(n, arr1, m, arr2)) {
             System.out.print(x + " ");
+        }
     }
 }
