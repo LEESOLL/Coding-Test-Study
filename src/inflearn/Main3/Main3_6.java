b972f0e31b7c7ff152d70d3bc6302a596d8531df
Lee Sol <leeso4567@naver.com>
2023-04-12 16:18:28 +0900

    [inflearn] 3-6. 최대 길이 연속부분수열 투포인터 알고리즘 풀이

--- src/inflearn/Main3/Main3_6.java
+++ src/inflearn/Main3/Main3_6.java
@@ -8,30 +8,22 @@ public class Main3_6 { // 인프런 3-6. 최대 길이 연속부분수열
     // 1로만 구성된 최대 길이의 연속부분수열 찾기(최대 길이 출력)
 
     static int solution(int n, int k, int[] arr) {
-        int max = Integer.MIN_VALUE;
-        int cnt = 0;
+        int answer = Integer.MIN_VALUE;
+        int lt = 0;
         int zeroCnt = 0;
 
-        for(int i = 0; i < n; i++) {
-            for (int j = i; j < n; j++) {
-                if(arr[j] == 0) {
-                    zeroCnt++;
-                    if(zeroCnt == k+1) {
-                        if(cnt > max) {
-                            max = cnt;
-                        }
-                        break;
-                    }
-                    cnt++;
-                } else if(arr[j] == 1) {
-                    cnt++;
+        for(int rt = 0; rt < n; rt++) {
+            if(arr[rt] == 0) zeroCnt++;
+            while(zeroCnt > k) {
+                if(arr[lt] == 0) {
+                    zeroCnt--;
                 }
+                lt++;
             }
-            zeroCnt = 0;
-            cnt = 0;
+            answer = Math.max(answer, rt - lt + 1); //rt-lt+1 은 선분의 길이
         }
 
-        return max;
+        return answer;
     }
 
     public static void main(String[] args) {
